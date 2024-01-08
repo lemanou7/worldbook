602522a9844564165908f81205dcf8835c777104
Drissa Bagate <lemanou7@yahoo.fr>
2024-01-08 00:16:30 -0700

    Main folder deleted and replaced

--- Main/settings.py
+++ Main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k2ddjrk7q@r5!d!*xj1&j89p1(&ob9x%sqe9kja3ec2nplfn%7'
+SECRET_KEY = 'django-insecure-%vkl+2o4u486fj_@gnbnccy=svtlq!gf4(bdvjbh_l5xq+rtod'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
